9228b8c206868a12c74f39c23491a966e6eb4426
hichem ali <ali.hichem@mail.com>
2023-05-10 19:52:45 +0100

    move smart contracts under 'contracts' folder and upgrade to pragma 0.8.19

--- contracts/ownable.sol
+++ contracts/ownable.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.25;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 /**
 * @title Ownable
@@ -6,7 +7,7 @@ pragma solidity ^0.4.25;
 * functions, this simplifies the implementation of "user permissions".
 */
 contract Ownable {
-  address private _owner;
+  address payable private _owner;
 
   event OwnershipTransferred(
     address indexed previousOwner,
@@ -17,15 +18,15 @@ contract Ownable {
   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
   * account.
   */
-  constructor() internal {
-    _owner = msg.sender;
+  constructor() {
+    _owner = payable(msg.sender);
     emit OwnershipTransferred(address(0), _owner);
   }
 
   /**
   * @return the address of the owner.
   */
-  function owner() public view returns(address) {
+  function owner() public view returns(address payable) {
     return _owner;
   }
 
@@ -52,7 +53,7 @@ contract Ownable {
   */
   function renounceOwnership() public onlyOwner {
     emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
+    _owner = payable(address(0));
   }
 
   /**
@@ -70,6 +71,6 @@ contract Ownable {
   function _transferOwnership(address newOwner) internal {
     require(newOwner != address(0));
     emit OwnershipTransferred(_owner, newOwner);
-    _owner = newOwner;
+    _owner = payable(newOwner);
   }
 }
--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -1,9 +1,14 @@
-pragma solidity ^0.4.25;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "./zombiefeeding.sol";
+import "./safemath.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  using SafeMath for uint256;
+  using SafeMath32 for uint32;
+  using SafeMath16 for uint16;
   uint levelUpFee = 0.001 ether;
 
   modifier aboveLevel(uint _level, uint _zombieId) {
@@ -12,7 +17,7 @@ contract ZombieHelper is ZombieFeeding {
   }
 
   function withdraw() external onlyOwner {
-    address _owner = owner();
+    address payable _owner = owner();
     _owner.transfer(address(this).balance);
   }
 
@@ -25,7 +30,7 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].level = zombies[_zombieId].level.add(1);
   }
 
-  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
+  function changeName(uint _zombieId, string memory _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
@@ -33,7 +38,7 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].dna = _newDna;
   }
 
-  function getZombiesByOwner(address _owner) external view returns(uint[]) {
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
     uint counter = 0;
     for (uint i = 0; i < zombies.length; i++) {
